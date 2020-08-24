696654552118b202f03087a499d342ab1888fc17
imran-shaikh-au6 <imranshaikh110120@gmail.com>
2020-08-24 12:39:26 +0530

    m

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json()).use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://peaceful-sammet-526422.netlify.app/",
+    })
+);
 const userRoutes = require("./Routes/userRoutes/userRoutes");
 const productRoutes = require("./Routes/productRoutes/productRoutes");
 
@@ -25,14 +29,14 @@ app.get("/", (req, res) => {
 app.use(userRoutes);
 app.use(productRoutes);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.listen(PORT, () => {
   console.log("server started");
 });
